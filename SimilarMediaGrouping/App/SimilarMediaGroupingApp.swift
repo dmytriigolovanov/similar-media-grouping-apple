5f120afb38507a5dfd66849878838d04904dda04
Dmytrii Golovanov <dmytrii.golovanov@gmail.com>
2026-03-12 10:50:21 +0200

    Dev: add base iOS app

---
+++ SimilarMediaGrouping/App/SimilarMediaGroupingApp.swift
@@ -0,0 +1,35 @@
+//
+//  SimilarMediaGroupingApp.swift
+//  SimilarMediaGrouping
+//
+//  Created by Dmytrii Golovanov on 12.03.2026.
+//  Copyright © 2026 Dmytrii Golovanov. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SimilarMediaGroupingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            // TODO: remove content view placeholder
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
